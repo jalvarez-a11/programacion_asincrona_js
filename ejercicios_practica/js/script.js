98d0bac33c48f5f56644d239b4876e5e1f4b6c97
jalvarez-a11 <139930695+jalvarez-a11@users.noreply.github.com>
2024-07-02 11:22:05 -0500

    Add files via upload

--- ejercicios_practica/js/script.js
+++ ejercicios_practica/js/script.js
@@ -2,3 +2,51 @@
 
 console.log("----------------------------------------");
 console.log("Fetch Poke JSON");
+
+const ejemplo = 1;
+
+if(ejemplo == 1) {
+
+// Fetch de un archivo JSON
+fetch("./bulbasaur.json")
+    .then(response => response.text())
+    .then(data => {
+        /** Procesar los datos **/
+        console.log("Datos procesados en formato ¿?");
+        console.log(data);
+}
+)
+    .catch(data => console.log("falla la promesa"))
+    .finally(d => console.log("se ejecuta siempre"));
+};
+
+const pokeNameDiv = document.getElementById('pokeName');
+const pokeTypesDiv = document.getElementById('pokeTypes');
+const pokeStatsDiv = document.getElementById('pokeStats');
+
+fetch('bulbasaur.json')
+  .then(response => response.json())
+  .then(pokemonData => {
+    displayPokemonName(pokemonData.name);
+    displayPokemonTypes(pokemonData.types);
+    displayPokemonStats(pokemonData.stats);
+  });
+
+function displayPokemonName(name) {
+  pokeNameDiv.innerHTML = `<h2>${name}</h2>`;
+}
+
+function displayPokemonTypes(types) {
+  const typeHTML = types.map(type => `<span class="type">${type.type.name}</span>`).join('');
+  pokeTypesDiv.innerHTML = `<h3>Tipos:</h3> ${typeHTML}`;
+}
+
+function displayPokemonStats(stats) {
+  const statHTML = stats.map(stat => `
+    <div class="stat">
+      <span class="stat-name">${stat.stat.name}:</span>
+      <span class="stat-value">${stat.base_stat}</span>
+    </div>
+  `).join('');
+  pokeStatsDiv.innerHTML = `<h3>Estadísticas:</h3> ${statHTML}`;
+}
